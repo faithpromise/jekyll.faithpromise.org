f1f68050e0485868fe23b856d3dba9c877d273d4
Brad Roberts <bradr@faithpromise.org>
2015-05-11 13:00:41 -0400

    Added input and output paths in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,18 @@ module.exports = function (grunt) {
     // Show elapsed time
     require('time-grunt')(grunt);
 
+    // Source JS files
+    var jsOutput_dev = 'build/main.dev.js',
+        jsOutput_production = 'build/main.min.js',
+        jsInput = [
+            '_js/main.js'
+        ];
+
+    // Source LESS files
+    var lessOutput_dev = 'build/main.dev.css',
+        lessOutput_production = 'build/main.min.css',
+        lessInput = ['_less/main.less'];
+
     // Project configuration.
     grunt.initConfig(
         {
