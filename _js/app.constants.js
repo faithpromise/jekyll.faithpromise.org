1d4e1b38799e5ec326e4e2a85190fa215698a31c
Brad Roberts <bradr@faithpromise.org>
2015-06-19 17:13:30 -0400

    Working on locations page

--- _js/app.constants.js
+++ _js/app.constants.js
@@ -4,4 +4,8 @@
         'AUTO_PLAY_VIDEO': 'auto-play-video'
     });
 
+    module.constant('SITE', {
+        'GOOGLE_MAPS_KEY': 'AIzaSyBkH5rBySTyP0S6q2nCFGOz3Mj2oBpNECw'
+    });
+
 }(angular.module('app')));
