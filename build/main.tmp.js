ffd93a18747d37df67cf31733120ac539494bba4
Brad Roberts <bradr@faithpromise.org>
2015-06-26 09:48:14 -0400

    Deploy

--- build/main.tmp.js
+++ build/main.tmp.js
@@ -2145,6 +2145,49 @@ angular.module("template/modal/window.html", []).run(["$templateCache", function
     }
 
 })(angular.module('app'));
+;(function (module) {
+    'use strict';
+
+    module.directive('cardGrid', directive);
+
+    function directive() {
+        return {
+            restrict: 'A',
+            link: Link,
+            scope: {}
+        };
+    }
+
+    function Link(scope, elem) {
+
+        var i,
+            paragraph,
+            heighest = 0,
+            textParagraphs = angular.element(elem).find('p');
+
+        for(i = 0; i < textParagraphs.length; i++) {
+
+            paragraph = angular.element(textParagraphs[i]);
+
+            if (paragraph.hasClass('Card-text')) {
+                heighest = Math.max(textParagraphs[i].offsetHeight, heighest);
+            }
+
+        }
+
+        for(i = 0; i < textParagraphs.length; i++) {
+
+            paragraph = angular.element(textParagraphs[i]);
+
+            if (paragraph.hasClass('Card-text')) {
+                paragraph.css('min-height', heighest + 'px');
+            }
+
+        }
+
+    }
+
+})(angular.module('app'));
 ;(function (module, Waypoint) {
     'use strict';
 
@@ -2265,6 +2308,32 @@ angular.module("template/modal/window.html", []).run(["$templateCache", function
     }
 
 })(angular.module('app'));
+;(function (module, angular) {
+    'use strict';
+
+    module.directive('linkTo', directive);
+
+    directive.$inject = ['$window'];
+
+    function directive($window) {
+        return {
+            restrict: 'A',
+            link: function(scope, elem, attr) {
+                if (! attr.linkTo) {
+                    return;
+                }
+                angular.element(elem).addClass('clickable');
+                elem.on('click', function(e) {
+                    if (e.target.tagName !== 'A') {
+                        $window.location.href = attr.linkTo;
+                    }
+                });
+            }
+        };
+    }
+
+})(angular.module('app'), angular);
+
 ;(function (module) {
     'use strict';
 
